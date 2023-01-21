7841afea95ca56473469a16f57da569ceb24b598
Patrick Tulskie <patricktulskie@gmail.com>
2023-01-21 15:50:05 -0500

    Add main class and specs

--- lib/concuss.rb
+++ lib/concuss.rb
@@ -2,6 +2,23 @@
 
 class Concuss
   class Error < StandardError; end
+
+  attr_reader :url, :file, :header_set, :headers, :test_string
+
+  def initialize(url:, file: nil, header_set: :all, test_string: nil)
+    @url = url
+    @file = file
+    @header_set = header_set
+    @test_string = test_string
+
+    @headers = Concuss::Headers.new(header_set: header_set, file: file).group
+  end
+
+  def attack!
+    runner = Concuss::Runner.new(headers: headers, url: url, test_string: test_string)
+
+    runner.run
+  end
 end
 
 require_relative "concuss/version"
--- spec/concuss_spec.rb
+++ spec/concuss_spec.rb
@@ -4,4 +4,35 @@ RSpec.describe Concuss do
   it "has a version number" do
     expect(Concuss::VERSION).not_to be nil
   end
+
+  describe '#initialize' do
+    it 'sets the attributes correctly' do
+      concuss = Concuss.new(
+        url: 'https://example.com',
+        file: 'example.txt',
+        header_set: :all,
+        test_string: 'example'
+      )
+      expect(concuss.url).to eq('https://example.com')
+      expect(concuss.file).to eq('example.txt')
+      expect(concuss.header_set).to eq(:all)
+      expect(concuss.test_string).to eq('example')
+      expect(concuss.headers).to eq(Concuss::Headers.new(header_set: :all).group)
+    end
+
+    it 'raises an error if url is not provided' do
+      expect { Concuss.new }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe '#attack!' do
+    it 'creates a new instance of Concuss::Runner' do
+      concuss = Concuss.new(url: 'https://example.com')
+      runner = double('runner')
+      expect(runner).to receive(:run)
+      expect(Concuss::Runner).to receive(:new).with(url: 'https://example.com', headers: Concuss::Headers.new(header_set: :all).group, test_string: nil).and_return(runner)
+      concuss.attack!
+    end
+  end
+
 end
